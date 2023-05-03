0979e2aef381e87376e46e05a60ccdb09ba5f864
StasSaltykov <stasyanych92@gmail.com>
2023-05-04 01:27:41 +0300

    imitator first try

--- src/main/java/com/storehouse/service/FullnessMeasurementImitatorImpl.java
+++ src/main/java/com/storehouse/service/FullnessMeasurementImitatorImpl.java
@@ -1,6 +1,8 @@
 package com.storehouse.service;
 
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.storehouse.model.Container;
 import com.storehouse.model.FullnessMeasurement;
@@ -8,11 +10,33 @@ import com.storehouse.model.FullnessMeasurement;
 @Service
 public class FullnessMeasurementImitatorImpl implements FullnessMeasurementImitator {
 	
+	@Value("${app.amount.containers: 10}")
+	int nContainers;
+	@Value("${app.min.percent: 0}")
+	int minPercent; 
+	@Value("${app.max.percent: 100}")
+	int maxPercent;
+	ThreadLocalRandom tlr = ThreadLocalRandom.current();
+	HashMap<Integer, Integer> containersStatus = new HashMap<>();
+	
 	@Override
 	public FullnessMeasurement nextMeasurement() {
-		ThreadLocalRandom tlr = ThreadLocalRandom.current();
-		Container container = new Container(tlr.nextInt(1, 5), "Apple", "Kilo", 1000);
-		return new FullnessMeasurement(container, tlr.nextInt(1, 100));
+		Container container = getContainer(tlr.nextInt(1, nContainers));
+		int status = getStatus(container.number);
+		return new FullnessMeasurement(container, status);
+	}
+
+	private int getStatus(int number) {
+		Integer value = containersStatus.get(number);
+		return value == null ? tlr.nextInt(minPercent, maxPercent) : getNewStatus(number);
+	}
+
+	private int getNewStatus(int number) {
+		return tlr.nextInt(minPercent, containersStatus.get(number));
+	}
+
+	private Container getContainer(int number) {
+		return new Container(number, "Apple", "Kilo", 1000);
 	}
 
 }
